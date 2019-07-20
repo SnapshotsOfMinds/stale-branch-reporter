0f209069baf34b6bf47bed05ee6c47ecbf51d79d
Christopher Bowen <christopher.bowen13@gmail.com>
2019-07-20 17:08:57 -0500

    Added toString for Repositories

--- src/main/java/com/github/report/object/Repository.java
+++ src/main/java/com/github/report/object/Repository.java
@@ -2,6 +2,9 @@ package com.github.report.object;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 /**
  * Object representation of a Repository.
@@ -24,5 +27,29 @@ public class Repository {
 
   public void setBranches(List<Branch> branches) {
     this.branches = branches;
+  }
+
+  @Override
+  public String toString() {
+    return "[{Repository} Name: " + name + ", Branches: " + branches.stream().map(Branch::toString).collect(Collectors.joining("\n"));
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (!(o instanceof Repository)) {
+      return false;
+    }
+
+    final Repository other = (Repository) o;
+    return Objects.equals(this.name, other.name) && Objects.equals(this.branches, other.branches);
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder().append(this.name).append(this.branches).toHashCode();
   }
 }
