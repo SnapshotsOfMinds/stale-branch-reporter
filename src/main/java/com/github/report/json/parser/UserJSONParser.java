1ea3685eb66a87d66dde7e613d43cb7e3fd303f3
Christopher Bowen <christopher.bowen13@gmail.com>
2019-07-20 19:28:18 -0500

    Fixed user name format

--- src/main/java/com/github/report/json/parser/UserJSONParser.java
+++ src/main/java/com/github/report/json/parser/UserJSONParser.java
@@ -20,8 +20,9 @@ public class UserJSONParser extends JSONParser<User> {
       JSONArray jsonArray = getJSONArray(jsonData);
       for (int i = 0; i < jsonArray.length(); i++) {
         JSONObject json = jsonArray.getJSONObject(i);
-        String[] ldap = json.getString("ldab_dn").split(",");
-        users.add(new User.Builder().login(json.getString("login")).name(ldap[0].substring(0, ldap[0].length() - 2) + " " + ldap[1]).build());
+        String[] ldap = json.getString("ldap_dn").split(",");
+        // Format of name in ldap array is CN=last\,first
+        users.add(new User.Builder().login(json.getString("login")).name(ldap[1] + " " + ldap[0].substring(3, ldap[0].length() - 1)).build());
       }
     } catch (JSONException e) {
       throw new JSONParseException(e);
